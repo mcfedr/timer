64743dcb52ad28db15f7668b2a9166213d349521
Fred Cox <mcfedr@gmail.com>
2014-06-23 22:38:02 +0300

    clean before deploy

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
 
-gulp.task('deploy', ['build'], function () {
+gulp.task('deploy', ['clean', 'build'], function () {
     gulp.src("./dist/**/*")
         .pipe($.ghPages());
 });
